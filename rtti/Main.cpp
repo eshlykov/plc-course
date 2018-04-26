023954da650a9cd34e1920fc6895266cdf4351ce
eshlykov <e.a.shlykov@gmail.com>
2018-04-26 17:48:09 +0300

    Значительные продвижения

--- rtti/Main.cpp
+++ rtti/Main.cpp
@@ -6,25 +6,40 @@
 Class( A ) {
 public:
 	EnableRTTI( A );
-	int ccc;
+	int a = 0;
 };
 
 Class( B ) {
 public:
 	EnableRTTI( B );
-	int ccccc;
+	int b = 1;
 };
 
-ClassExtends( C, A, B ) {
+ClassExtends( C, public A, public B ) {
 public:
 	EnableRTTI( C );
-	int c = 17;
+	int c = 2;
 };
 
 int main()
 {
-	C c;
-	auto typeinfo = TypeId( c );
-	std::cout << typeinfo.Name << " " << typeinfo.HashCode << std::endl;
+	A* ptrC = new C;
+	A* ptrA = new A();
+
+	if( DynamicCast( C*, ptrC ) != nullptr ) {
+		std::cout << "Can be casted to C*" << std::endl;
+	} else {
+		std::cout << "Cannot be casted to C*" << std::endl;
+	}
+	if( DynamicCast( B*, ptrC ) != nullptr ) {
+		std::cout << "Can be casted to B*" << std::endl;
+	} else {
+		std::cout << "Cannot be casted to B*" << std::endl;
+	}
+	if( DynamicCast( B*,  ptrA ) != nullptr ) {
+		std::cout << "Can be casted to B*" << std::endl;
+	} else {
+		std::cout << "Cannot be casted to B*" << std::endl;
+	}
 	return 0;
 }
--- rtti/TypeInfo.h
+++ rtti/TypeInfo.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <cctype>
 #include <functional>
 #include <iterator>
@@ -8,15 +9,16 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <iostream>
 
 class CTypeId {
 public:
-	const std::string Name;
-	const int HashCode;
+	std::string Name;
+	std::size_t HashCode;
 
-	CTypeId() = delete;
+	CTypeId() = default;
 
-	CTypeId( std::string name, int hashCode ) :
+	CTypeId( std::string name, std::size_t hashCode ) :
 		Name( name ),
 		HashCode( hashCode )
 	{
@@ -25,101 +27,108 @@ public:
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::unordered_set<char> openBracket{ '(', '[', '{', '<' };
-std::unordered_set<char> closeBracket{ ')', ']', '}', '>' };
+class CTypeIdStorage {
+public:
+	CTypeId TypeId;
+};
 
-bool Is( const char c, const std::unordered_set<char>& set )
+//----------------------------------------------------------------------------------------------------------------------
+
+std::unordered_set<char> OpenBracket{ '(', '[', '{', '<' };
+std::unordered_set<char> CloseBracket{ ')', ']', '}', '>' };
+
+bool GetBalance( const char c)
 {
-	return set.find( c ) != set.end();
-}
-
-std::string GetClassName( std::istringstream& stream, const std::string& prefix = "" )
-{
-	// The problem is classes like CSomeClass<CClass1, CClass2<CClass3>, std::string>,
-	// that's why we need to count balance.
-	int balance = 0;
-
-	for( const auto& c : prefix ) {
-		// No need to divide by types of brackets because the sequence is definitely correct.
-		balance += Is( c, openBracket ) ? 1 : ( Is( c, closeBracket ) ? -1 : 0);
-	}
-
-	std::string className = prefix;
-
-	char c;
-	while( stream.get( c ) ) {
-		if( std::isspace( c ) ) {
-			continue;
-		}
-		if( c == ',' && balance == 0 ) {
-			return className;
-		}
-		// If balance is not zero, the next char will be part of class name even when char is comma.
-		className += c;
-		balance += Is( c, openBracket ) ? 1 : ( Is( c, closeBracket ) ? -1 : 0 );
-	}
-
-	return className;
+	// Do not need to check types of brackets because of the sequence of bracket is definitely correct.
+	return ( OpenBracket.find( c ) != OpenBracket.end() ) ? 1 : ( CloseBracket.find( c ) != CloseBracket.end() ? -1 : 0);
 }
 
 std::vector<std::string> ExtractClassNamesList( std::string text )
 {
 	std::istringstream stream{ text };
 	std::vector<std::string> words{};
-	std::string word;
-	std::string className = "";
+	std::string word = "";
+	char current;
+	int balance = 0;
 
-	while( stream >> word ) {
-		if( word == "public" || word == "protected" || word == "private" || word == "virtual" ) {
-			// Next token is a class name.
-			className = GetClassName( stream );
-		} else if( word == "virtual" ) {
-			throw std::logic_error( "RTTI does not support virtual inheritance." );
-		} else {
-			// Default private, so we just read part of class name.
-			className = GetClassName( stream, word );
-		}
-		if( className != "" ) {
-			words.push_back( className );
+	while( stream.get( current ) ) {
+		balance += GetBalance( current );
+		if( current == ',' && balance == 0 ) {
+			if( word != "" ) {
+				// Now we are sure that word is some class name.
+				words.push_back( word );
+			}
+			word = "";
+		} else if( std::isspace( current ) && balance == 0 ) {
+			if( word == "private" || word == "protected" || word == "public" ) {
+				word = "";
+			} else if( word == "virtual" ) {
+				throw std::logic_error( "RTTI does not support virtual inheritance" );
+			}
+		} else if( word != "" || !std::isspace( current ) ) {
+			// Don't write first space in the class name.
+			word += current;
 		}
 	}
-	if( className != "" ) {
-		words.push_back( className );
+	if( word != "" ) {
+		words.push_back( word );
 	}
-
 	return words;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-std::unordered_map<std::string, int> ClassSizes;
-std::unordered_map<std::string, std::vector<std::string>> ClassParents;
+std::unordered_map<std::string, std::size_t> ClassSizes;
 std::unordered_map<std::string, std::vector<std::string>> ClassChildren;
 
 void* UpdateTree( std::string child, std::string parents = "" )
 {
-	ClassParents[child] = ExtractClassNamesList( parents );
-	for( const auto& parent : ClassParents[child] ) {
-		ClassChildren[parent].push_back( child );
-	}
+	//auto parentsList = ExtractClassNamesList( parents );
+	ClassChildren[child] = ExtractClassNamesList( parents );
+	//for( const auto& parent : parentsList ) {
+		//ClassChildren[child].push_back( parent );
+	//}
 	return nullptr;
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-bool IsCastable( std::string from, std::string to )
+bool IsCastable( std::string base, std::string target )
 {
-	return true;
+	if( target.size() > 0 && target[target.size() - 1] == '*' ) {
+		// Remove ending *.
+		std::string dereferencedType;
+		std::copy_n( target.begin(), target.size() - 1, std::back_inserter( dereferencedType ) );
+		std::swap( target, dereferencedType );
+	}
+	if( base == target ) {
+		return true;
+	}
+	if( ClassSizes[base] == 0 || ClassSizes[target] == 0 ) {
+		// RTTI is not enabled or enabled in private section.
+		return false;
+	}
+
+	for( const auto& child : ClassChildren[base] ) {
+		if( child == target || IsCastable( child, target ) ) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
-int CountShift( std::string from, std::string to )
+std::size_t CountShift( std::string base, std::string target )
 {
-	int shift = sizeof( CTypeId );
-	for( const auto& parent : ClassParents[from] ) {
-		if( parent == to ) {
-			break;
+	std::size_t shift = sizeof( CTypeIdStorage );
+	for( const auto& child : ClassChildren[base] ) {
+		if( child == target ) {
+			return shift;
+		} else if ( IsCastable( child, target ) ) {
+			return shift + CountShift( child, target );
+		} else {
+			shift += ClassSizes[child];
 		}
-		shift += ClassSizes[parent];
 	}
 	return shift;
 }
@@ -128,19 +137,19 @@ int CountShift( std::string from, std::string to )
 
 #define Class( Class ) \
 void* Class##Tree = UpdateTree( #Class ); \
-class Class
+class Class : virtual public CTypeIdStorage
 
 #define ClassExtends( Class, ... ) \
 void* Class##Tree = UpdateTree( #Class, #__VA_ARGS__ ); \
-class Class : __VA_ARGS__
+class Class : virtual public CTypeIdStorage, __VA_ARGS__
 
 #define EnableRTTI( Class ) \
-const CTypeId TypeId{ #Class, std::hash<std::string>{}( #Class ) };\
-const void* Class##Size = []{ \
+void* Class##RTTI = [this]{ \
+	TypeId = CTypeId{ #Class, std::hash<std::string>{}( #Class ) };\
 	ClassSizes[#Class] = sizeof( Class ); \
 	return nullptr; \
 }();
 
-#define TypeId( object ) object.TypeId
+#define TypeId( object ) ( (object).TypeId )
 
-#define DynamicCast( Type, Ptr ) ( IsCastable( TypeId( *Ptr ).Name, #Type ) ? static_cast<Type>( Ptr + CountShift( TypeId( *Ptr ).Name, #Type ) ) : nullptr )
+#define DynamicCast( Type, Ptr ) ( IsCastable( TypeId( *Ptr ).Name, #Type ) ?  reinterpret_cast<Type>( Ptr + CountShift( TypeId( *Ptr ).Name, #Type ) ) : nullptr )

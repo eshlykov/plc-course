b32ee2dda0bd54dd606dd7fa2e7a16525d2b5ec5
eshlykov <e.a.shlykov@gmail.com>
2018-05-02 01:42:57 +0300

    Пофиксил линковку

--- future/Future.h
+++ future/Future.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <atomic>
+#include <exception>
 #include <mutex>
 #include <condition_variable>
 
@@ -12,6 +13,7 @@ class CFuture {
 	friend CPromise<T>;
 
 public:
+	CFuture() = default;
 	CFuture( CFuture<T>&& other );
 
 	T& Get();
@@ -25,13 +27,13 @@ private:
 	};
 
 	T value{};
-	std::exception_ptr exceptionPtr{};
+	std::exception exception{};
 	T_FutureState state{ FS_Waiting };
 	std::mutex mutex{};
 	std::condition_variable isFinished{};
 
 	void setValue( T&& _value );
-	void setException( std::exception_ptr _exceptionPtr );
+	void setException( const std::exception& _exception );
 };
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -41,10 +43,80 @@ class CPromise {
 public:
 	CFuture<T>& GetFuture();
 	void SetValue( T&& value );
-	void SetException( std::exception_ptr exceptionPtr );
+	void SetException( const std::exception& exception );
 
 private:
 	CFuture<T> future{};
 };
 
 //----------------------------------------------------------------------------------------------------------------------
+
+template<class T>
+CFuture<T>::CFuture( CFuture<T>&& other )
+{
+	value = std::move( other.value );
+	state = other.state;
+}
+
+template<class T>
+T& CFuture<T>::Get()
+{
+	std::unique_lock<std::mutex> lock;
+	isFinished.wait( lock, [&] { return state != FS_Waiting; } );
+	if( state == FS_HasException ) {
+		throw exception;
+	}
+	return value;
+}
+
+template<class T>
+bool CFuture<T>::TryGet( T& result )
+{
+	std::lock_guard<std::mutex> lock( mutex );
+	if( state == FS_HasValue ) {
+		result = value;
+		return false;
+	}
+	if( state == FS_HasException ) {
+		throw exception;
+	}
+	return false;
+}
+
+template<class T>
+void CFuture<T>::setValue( T&& _value )
+{
+	std::lock_guard<std::mutex> lock( mutex );
+	value = std::move( _value );
+	state = FS_HasValue;
+	isFinished.notify_one();
+}
+
+template<class T>
+void CFuture<T>::setException( const std::exception& _exception )
+{
+	std::lock_guard<std::mutex> lock( mutex );
+	exception = _exception;
+	state = FS_HasException;
+	isFinished.notify_one();
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+template<class T>
+CFuture<T>& CPromise<T>::GetFuture()
+{
+	return future;
+}
+
+template<class T>
+void CPromise<T>::SetValue( T && value )
+{
+	future.setValue( std::move( value ) );
+}
+
+template<class T>
+void CPromise<T>::SetException( const std::exception& exception )
+{
+	future.setException( exception );
+}
